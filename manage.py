01ae660bc1c70946cbeec9400272f3bfbc411327
unknown <vishalkale@example.com>
2026-03-02 08:06:44 +0530

    Fix python path for Render deployment

--- manage.py
+++ manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sunshineresort.settings')
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+    os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "sunshineresort.settings"
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
